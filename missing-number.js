1be62999dec9581668164284010c8b7f27d93f1e
nimplay <nimrod7day@gmail.com>
2022-10-02 15:51:35 -0400

    formating

--- missing-number.js
+++ missing-number.js
@@ -1,3 +1,6 @@
+/* Given an array nums containing n distinct numbers in the range [0, n],
+return the only number in the range that is missing from the array. */
+
 var missingNumber = function(nums) {
     let sum = 0
     let total = 0
