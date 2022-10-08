bd3d22b9c55ef407e674ec30cf94dcfb32f2b4ef
nimplay <nimrod7day@gmail.com>
2022-10-08 13:48:22 -0400

    climbing-stairs

---
+++ climbing-stairs.js
@@ -0,0 +1,15 @@
+/* You are climbing a staircase. It takes n steps to reach the top.
+Each time you can either climb 1 or 2 steps. In how many distinct
+ways can you climb to the top? */
+
+var climbStairs = function(n) {
+    let arr=[];
+ arr[1]=1;
+ arr[2]=2;
+ for(let i=3;i<=n;i++){
+     arr[i]=arr[i-1]+arr[i-2];
+ }
+ return arr[n];
+};
+
+console.log(climbStairs(3))
